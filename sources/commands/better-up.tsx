d53a02ad39d2bfe28b8b8f6813ed0481ec45b043
Hyeseong Kim <cometkim.kr@gmail.com>
2020-03-16 02:27:09 +0900

    Migrate build environment from babel to @yarnpkg/builder

---
+++ sources/commands/better-up.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Box, render } from 'ink';
+import { Command, Usage } from 'clipanion';
+import { BaseCommand } from '@yarnpkg/cli';
+import { Configuration, Project, StreamReport, Cache } from '@yarnpkg/core';
+
+enum SettingKey {
+  LOCKFILE_FILENAME = 'lockfileFilename',
+  NPM_REGISTRY_SERVER = 'npmRegistryServer',
+}
+
+export default class UpCommand extends BaseCommand {
+  static usage: Usage = Command.Usage({
+    description: ``,
+    details: ``,
+    examples: [
+    ],
+  })
+
+  @Command.Path('better-up')
+  async execute(): Promise<number | void> {
+    const configuration = await Configuration.find(this.context.cwd, this.context.plugins);
+    const {project, workspace} = await Project.find(configuration, this.context.cwd);
+    const cache = await Cache.find(configuration);
+    // console.log(configuration);
+    // console.log(workspace);
+    // workspace?.manifest
+    render(
+      <Box>Hello world!!</Box>
+    );
+    return 0;
+  }
+}
