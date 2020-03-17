02345a25d8a7cf7ffe731b97f9f64d9c8a1380ba
Hyeseong Kim <cometkim.kr@gmail.com>
2020-03-18 01:52:51 +0900

    Setup ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,29 +9,12 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
   ],
   rules: {
-    'max-len': ['error', 80],
-    'comma-spacing': ['error', { after: true }],
-    'array-bracket-newline': ['error', 'consistent'],
-    'object-curly-newline': ['error', {
-      'ObjectExpression': 'always',
-      'ObjectPattern': {
-        'multiline': true,
-      },
-      'ImportDeclaration': 'never',
-      'ExportDeclaration': {
-        'multiline': true,
-        'minProperties': 3,
-      }
-    }],
-    'object-curly-spacing': ['error', 'always'],
-    'comma-dangle': ['error', 'always-multiline'],
     'indent': 'off',
     '@typescript-eslint/indent': ['error', 2],
     'semi': 'off',
     '@typescript-eslint/semi': ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
     'quotes': 'off',
     '@typescript-eslint/quotes': ['error', 'single'],
-    '@typescript-eslint/array-type': ['error', { default:'array-simple' }],
-    '@typescript-eslint/explicit-function-return-type': 'off',
   },
 };
