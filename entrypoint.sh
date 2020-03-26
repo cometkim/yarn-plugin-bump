478c7639d3008be7a179775c1d1cd7600da63a71
GitHub Action <action@github.com>
2020-03-26 22:06:06 +0000

    Bump-up dependencies

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,32 +2,18 @@
 
 set -uo pipefail
 
-if [[ -z "$GITHUB_TOKEN" ]]; then
-    echo '$GITHUB_TOKEN must be provided'
-    exit 1
+# Checking if the command pre-installed
+yarn bump --help > /dev/null
+if [[ $? -ne 0 ]]; then
+  yarn import "$PLUGIN_REMOTE"
 fi
+
+yarn bump $@ || exit 1
+
+REPOSITORY="https://:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
 git config --local user.name "$GIT_USERNAME"
 git config --local user.email "$GIT_EMAIL"
-
-git remote set-url origin "https://:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-
-# Checkout the target branch
-git fetch origin
-git checkout -b "$GIT_BRANCH" "origin/$GIT_BRANCH" 2>/dev/null || git checkout -b "$GIT_BRANCH" "origin/$GIT_BASE_REF"
-
-# Rebase
-git rebase -X theirs "origin/$GIT_BASE_REF"
-
-# Checking if the command pre-installed
-yarn bump --help >/dev/null 2>&1
-if [[ $? -ne 0 ]]; then
-  echo "Download plugin from $PLUGIN_REMOTE"
-  yarn plugin import "$PLUGIN_REMOTE"
-fi
-
-yarn bump $@
-
-# Commit & force-push updates
 git commit -a -m "$GIT_COMMIT_MSG"
-git push -uf origin "HEAD:$GIT_BRANCH"
+
+git push $REPOSITORY HEAD:$GIT_BRANCH
