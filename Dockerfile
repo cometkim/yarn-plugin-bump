4c5042d7eb5e0f0c9cc00185a11de324c84f423e
Hyeseong Kim <cometkim.kr@gmail.com>
2020-03-27 04:37:44 +0900

    Fix action

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:alpine
+FROM node:13.1
 
 LABEL repository="https://github.com/cometkim/yarn-plugin-bump"
 LABEL maintainer="Hyeseong Kim <cometkim.kr@gmail.com>"
@@ -6,17 +6,15 @@ LABEL maintainer="Hyeseong Kim <cometkim.kr@gmail.com>"
 ENV PLUGIN_VERSION="0.0.3"
 ENV PLUGIN_REMOTE="https://github.com/cometkim/yarn-plugin-bump/releases/download/v0.0.1/plugin-bump.js"
 
-# TODO pre-installed script
-# ENV PLUGIN_LOCAL=""
-
-RUN apk add --no-cache \
-  git \
-  bash \
-  ca-certificates
+RUN apt update && apt install -y --no-install-recommends \
+    ca-certificates \
+    git \
+  && apt autoclean \
+  && apt autoremove -y \
+  && rm -rf /var/lib/apt/lists/*
 
 WORKDIR /action
 COPY entrypoint.sh .
 
-RUN chmod +x entrypoint.sh
-
+RUN chmod u+x /action/entrypoint.sh
 ENTRYPOINT ["/action/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,22 +7,27 @@ if [[ -z "$GITHUB_TOKEN" ]]; then
     exit 1
 fi
 
+git config --local user.name "$GIT_USERNAME"
+git config --local user.email "$GIT_EMAIL"
+
+git remote set-url origin "https://:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
 # Checkout the target branch
-git checkout $GIT_BRANCH 2> /dev/null || git checkout -b $GIT_BRANCH
+git fetch origin
+git checkout -b "$GIT_BRANCH" "origin/$GIT_BRANCH" 2>/dev/null || git checkout -b "$GIT_BRANCH" "origin/$GIT_BASE_REF"
+
+# Rebase
+git rebase -X theirs "origin/$GIT_BASE_REF"
 
 # Checking if the command pre-installed
-yarn bump --help > /dev/null 2>&1
+yarn bump --help >/dev/null 2>&1
 if [[ $? -ne 0 ]]; then
   echo "Download plugin from $PLUGIN_REMOTE"
   yarn plugin import "$PLUGIN_REMOTE"
 fi
 
-yarn bump $@ || exit 1
+yarn bump $@
 
-REPOSITORY="https://:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-
-git config --local user.name "$GIT_USERNAME"
-git config --local user.email "$GIT_EMAIL"
+# Commit & force-push updates
 git commit -a -m "$GIT_COMMIT_MSG"
-
-git push $REPOSITORY HEAD:$GIT_BRANCH
+git push -uf origin "HEAD:$GIT_BRANCH"
